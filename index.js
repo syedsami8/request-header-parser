0fedf1c90c2735a22c3d74b8e83780508a432618
Syed Samiuddin <ssamiuddin007@gmail.com>
2018-06-14 15:33:09 +0530

    update to CORS

--- index.js
+++ index.js
@@ -4,11 +4,11 @@ var app = express();
 app.set('port',(process.env.PORT || 5000));
 app.use(express.static(__dirname + '/public'));
 
-app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  next();
- });
+app.use(function(req, res, next) {
+      res.header("Access-Control-Allow-Origin", "*");
+      res.header("Access-Control-Allow-Headers", "X-Requested-With");
+      next();
+    });
 
 app.get('/api/whoami', function (req, res) {
     
